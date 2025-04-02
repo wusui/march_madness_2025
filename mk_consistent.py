daf359e9a5f3d22c873bec9840d1baebcba2eb6e
Warren Usui <warrenusui@earthlink.net>
2025-04-01 21:50:11 -0700

    Use script.txt file to find name to abbrev links

--- mk_consistent.py
+++ mk_consistent.py
@@ -5,68 +5,27 @@ used by the bracket pages
 """
 import os
 import json
-from itertools import chain
 
-def get_real(semireality):
+def mk_xlate_tbl():
     """
-    Associate team abbreviation with full name used in brackets
+    Make a table of abbreviations indexed by name using *_script.txt information
     """
-    def find_exact_abbrv(tpairs):
-        def fes_inner(spair):
-            def fes_inn2(abbrev):
-                def fes_inn3(sname):
-                    def matches():
-                        #if sname.lower().startswith(abbrev.lower()):
-                        #    return True
-                        if abbrev.lower() in sname.lower():
-                            return True
-                        tminits = ''.join(list(map(lambda a: a[0],
-                                                      sname.split())))
-                        if abbrev in [tminits + 'U', 'U' + tminits, tminits]:
-                            return True
-                        ncomp = list(map(lambda a: sname.lower().find(a),
-                                       list(abbrev.lower())))
-                        if len(abbrev) > 2:
-                            if len(list(filter(lambda a: a >= 0,
-                                        ncomp))) == len(abbrev):
-                                return True
-                            if abbrev.endswith('U'):
-                                if len(list(filter(lambda a: a >= 0,
-                                        ncomp[0:-1]))) == len(abbrev) - 1:
-                                    return True
-                            if abbrev.startswith('U'):
-                                if len(list(filter(lambda a: a >= 0,
-                                        ncomp[1:]))) == len(abbrev) - 1:
-                                    return True
-                        return False
-                    if matches():
-                        return [sname, abbrev]
-                    return []
-                return list(map(fes_inn3, spair[1]))
-            rmtch = list(filter(None, list(map(fes_inn2, spair[0]))))
-            fmtch = list(map(lambda a: list(filter(None, a)), rmtch))
-            nmtch = list(filter(None, fmtch))
-            if nmtch:
-                nmtch = list(map(lambda a: a[0], nmtch))
-                if len(nmtch) == 1 and len(spair[0]) == 2:
-                    sc1 = list(filter(lambda a: a != nmtch[0][0], spair[1]))[0]
-                    ab1 = list(filter(lambda a: a != nmtch[0][1], spair[0]))[0]
-                    nmtch += [[sc1, ab1]]
-            else:
-                print('Consistency Error: ', spair)
-            return nmtch
-        return list(map(fes_inner, tpairs))
+    def get_ab_nm(sloc):
+        def get_field(bdata):
+            field = sdata[0:bdata].rfind('":') + 2
+            relv_data = sdata[field:field + 100]
+            relv_data = relv_data[relv_data.find('":"') + 3:]
+            relv_data = relv_data[0:relv_data.find(',') - 1]
+            return relv_data
+        abbrev = get_field(sdata[0:sloc].rfind('"abbrev":'))
+        name = get_field(sloc + sdata[sloc:].find('"name":'))
+        return [name, abbrev]
     prefix = os.getcwd().split(os.sep)[-1]
-    with open(f'{prefix}_brackets.json', 'r', encoding='utf-8') as pdata:
-        pinfo = json.load(pdata)
-    tlists = list(map(lambda a: pinfo[a], pinfo))
-    ftlists = list(filter(lambda a: len(a) > 0, tlists))
-    szlists = list(map(lambda a: a[0:32], ftlists))
-    grp_tms = list(zip(*szlists))
-    ptms = list(map(lambda a: list(set(list(a))), grp_tms))
-    tpairs = list(zip(ptms, semireality[0:32]))
-    matches = find_exact_abbrv(tpairs)
-    return dict(list(chain.from_iterable(matches)))
+    with open(f'{prefix}_script.txt', 'r', encoding='utf-8') as afile:
+        sdata = afile.read()
+    matchp = [i for i in range(len(sdata)) if sdata.startswith(
+        '"matchupPosition":', i)]
+    return dict(list(map(get_ab_nm, matchp)))
 
 def mk_consistent():
     """
@@ -83,7 +42,7 @@ def mk_consistent():
     if len(semireality) < 32:
         return len(semireality)
     if not link_info:
-        link_info = get_real(semireality)
+        link_info = mk_xlate_tbl()
     new_reality = list(map(lambda a: link_info[a[0]], semireality))
     if not os.path.isfile(lfile):
         json_data = json.dumps(link_info, indent=4)
